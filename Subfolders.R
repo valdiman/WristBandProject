b220ad30223cb42689da080d950c54192fe365c8
martie <andres-martinez@uiowa.edu>
2023-03-20 10:03:03 -0500

    initial version

---
+++ Subfolders.R
@@ -0,0 +1,8 @@
+# Create subfolder within the current working directory
+dir.create("R")
+dir.create("Data")
+dir.create("Output")
+dir.create("Output/Data")
+dir.create("Output/Data/csv")
+dir.create("Output/Data/excel")
+dir.create("Output/Plots")
