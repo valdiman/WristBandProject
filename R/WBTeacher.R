46b6c2bf61ca5079f6c420a2376a7901577030ea
martie <andres-martinez@uiowa.edu>
2023-03-27 15:44:17 -0500

    Include PCB profile plots and analysis.

--- R/WBTeacher.R
+++ R/WBTeacher.R
@@ -22,7 +22,7 @@ bl <- data.frame(read_excel("Data/BlanksWBT.xlsx", sheet = "blanks",
 # Remove entire "bad/yellow" transition
 s.1 <- data.frame(read_excel("Data/SamplesWBT.xlsx", sheet = "samples",
                             col_names = TRUE, col_types = NULL))
-s.2 <- data.frame(read_excel("Data/SamplesWBT.xlsx", sheet = "samplesv02",
+s.n <- data.frame(read_excel("Data/SamplesWBT.xlsx", sheet = "samplesv02",
                              col_names = TRUE, col_types = NULL))
 
 # Distribution analysis ---------------------------------------------------
@@ -91,23 +91,109 @@ write.csv(normality, file = "Output/Data/csv/normality.csv")
 
 # Calculate LOQ -----------------------------------------------------------
 # Create LOQ, i.e., upper 95 CI% (mean + 1.96*sd/(n)^0.5)
-loq <- colMeans(bl.1) + 1.96*sapply(bl.1, sd)/sqrt(20)
+loq <- colMeans(bl.1) + 1.96*sapply(bl.1, sd)/sqrt(21)
 loq <- data.frame(t(loq))
 
-# Make comparison between s.1 and loq
+# Sample loq comparison ---------------------------------------------------
 # If s.1 > loq, then s.1, if not 0
+# Remove sample names from s.1
+s.1.1 <- s.1[, 2:174]
 # Create matrix to storage s.1 or loq values in s.2
- <- matrix(NA, nrow = dim(s.1)[1], ncol = dim(s.1)[2])
+s.2 <- matrix(NA, nrow = dim(s.1.1)[1], ncol = dim(s.1.1)[2])
 # Do comparison
-for(i in 1:dim(s.1)[1]) {
-  for(j in 1:dim(s.1)[2]) {
-    if (log10(s.1[i, j]) > loq[j]) {
-      s.2[i, j] <- s.1[i, j]
+for(i in 1:dim(s.1.1)[1]) {
+  for(j in 1:dim(s.1.1)[2]) {
+    if (s.1.1[i, j] > loq[j]) {
+      s.2[i, j] <- s.1.1[i, j]
     } else {
       s.2[i, j] <- 0
     }
   }
 }
 
+# Transform to data.frame
+s.2 <- data.frame(s.2)
+# Add sample
+s.2 <- cbind(s.1$Congener.Sample, s.2)
+# Add column names
+colnames(s.2) <- colnames(s.1)
 
+# Plot tPCB ---------------------------------------------------------------
+ggplot(s.2, aes(y = rowSums(s.2[, 2:174]), x = factor(Congener.Sample))) +
+  geom_bar(stat = 'identity', width = 0.8, fill = "black") +
+  theme_bw() +
+  theme(aspect.ratio = 6/35) +
+  ylab(expression(Sigma*"PCB (ng/g)")) +
+  xlab(expression("")) +
+  theme(axis.text.y = element_text(face = "bold", size = 7),
+        axis.title.y = element_text(face = "bold", size = 7)) +
+  theme(axis.text.x = element_text(face = "bold", size = 7,
+                                   angle = 60, hjust = 1),
+        axis.title.x = element_text(face = "bold", size = 7))
 
+# Profile Analysis --------------------------------------------------------
+# Generate profile
+tmp <- rowSums(s.1.1, na.rm = TRUE)
+prof <- sweep(s.1.1, 1, tmp, FUN = "/")
+# Include sample names
+prof <- cbind(s.1$Congener.Sample, prof)
+# Transpose
+prof <- t(prof)
+# Include PCB names
+prof <- cbind(row.names(prof), prof)
+# Add sample names as column names
+colnames(prof) <- prof[c(1),]
+# Delete first row with sample names
+prof <- prof[-c(1),]
+# Delete row names
+rownames(prof) <- NULL
+# Transform to data.frame
+prof <- data.frame(prof)
+# Change first column name to congener
+colnames(prof)[1] <- c('congener')
+# Transform congener column to factor
+prof$congener <- as.factor(prof$congener)
+# Convert all character columns to numeric
+prof[, 2:37] <- as.data.frame(apply(prof[, 2:37], 2, as.numeric))
+# Organize PCB congeners
+prof$congener <- factor(prof$congener, levels = unique(prof$congener))
+
+# Plots
+ggplot(prof, aes(y = prof[, 37], x = congener)) +
+  geom_bar(stat = 'identity', width = 0.8, fill = "black") +
+  theme_bw() +
+  ylim(0, 0.08) +
+  theme(aspect.ratio = 6/35) +
+  ylab(expression(bold("Mass fraction "*Sigma*"PCB"))) +
+  xlab(expression("")) +
+  theme(axis.text.y = element_text(face = "bold", size = 8),
+        axis.title.y = element_text(face = "bold", size = 8)) +
+  theme(axis.text.x = element_text(face = "bold", size = 6,
+                                   angle = 60, hjust = 1))
+
+# Cosine theta ------------------------------------------------------------
+# Remove congener column
+prof.cos <- prof[, 2:37]
+# Create matrix to storage results
+costheta <- matrix(nrow = length(prof.cos[1,]),
+                      ncol = length(prof.cos[1,]))
+
+# Perform Cosine Theta
+for (i in 1:length(prof.cos[1,])) {
+  for (j in 1:length(prof.cos[1,])) {
+    m1 <- prof.cos[,i]
+    m2 <- prof.cos[,j]
+    costheta[i,j] <- sum(m1*m2)/(sum(m1^2)*sum(m2^2))^0.5
+  }
+}
+
+# 3 significant figures
+costheta <- formatC(signif(costheta, digits=3), digits=3)
+# Remove upper diagonal values
+costheta[upper.tri(costheta)] <- NA
+# Add name to columns
+colnames(costheta) <- colnames(prof.cos)
+# Add names to rows
+rownames(costheta) <- colnames(prof.cos)
+# Export data
+write.csv(costheta, file = "Output/Data/csv/costheta.csv")
