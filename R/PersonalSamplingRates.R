13fda56aba868056cbb177044a936ddcf2132554
martie <andres-martinez@uiowa.edu>
2024-01-15 19:50:46 -0600

    added new lines.

--- R/PersonalSamplingRates.R
+++ R/PersonalSamplingRates.R
@@ -13,12 +13,21 @@ install.packages("ggplot2")
   library(gridExtra)
 }
 
-# Read data from excel
+# Read data from excel ----------------------------------------------------
 data.amanda <- data.frame(read_excel("Data/Amanda.xlsx", sheet = "Sheet1",
                              col_names = TRUE, col_types = NULL))
 data.kay <- data.frame(read_excel("Data/Kay.xlsx", sheet = "Sheet1",
                                      col_names = TRUE, col_types = NULL))
 data.yau <- data.frame(read_excel("Data/Yau.xlsx", sheet = "Sheet1",
                                      col_names = TRUE, col_types = NULL))
+
+# Calculate personal sampling rate Amanda ---------------------------------
+# Select WBs to calculate air concentration
+data.amanda.1 <- data.amanda[1:3,]
+
+
+
+# Remove medata
+data.amanda.2 <- subset(data.amanda.1, select = -c(time.day:congeners))
 
 
