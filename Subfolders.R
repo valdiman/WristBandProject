7c6bc15b427435278295f0d4350cf2a91435f7ab
martie <andres-martinez@uiowa.edu>
2024-12-18 20:52:42 -0600

    updated version

--- Subfolders.R
+++ Subfolders.R
@@ -1,5 +1,7 @@
 # Create subfolder within the current working directory
 dir.create("R")
+dir.create("R/AirConcentration")
+dir.create("R/SamplingRates")
 dir.create("Data")
 dir.create("Output")
 dir.create("Output/Data")
