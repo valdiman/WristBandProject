c47c90537bb21a370035f0e2580caffe5f63015b
martie <andres-martinez@uiowa.edu>
2025-12-08 19:54:23 -0600

    updated version.

--- R/Profiles/ProfilesStudy1.R
+++ R/Profiles/ProfilesStudy1.R
@@ -8,6 +8,8 @@ install.packages("tidyr")
 install.packages("dplyr")
 install.packages("tibble")
 install.packages("ggpubr")
+install.packages("lsa")
+install.packages("SnowballC")
 
 # Load libraries
 {
@@ -17,9 +19,9 @@ install.packages("ggpubr")
   library(dplyr)
   library(tibble)
   library(ggpubr) # ggarrange
+  library(lsa) # cosine theta function
 }
 
-# Sampling rates calculations under static conditions ---------------------
 # Read data
 {
   PUF <- read.csv("Data/PUF.csv") # ng/m3
@@ -33,12 +35,11 @@ WB.1 <- subset(WB, select = -c(sample:time))
 # Create PCB profiles -----------------------------------------------------
 # (1) PUF
 # Compute row sums
-row_totals <- rowSums(PUF.1, na.rm = TRUE)
+tmp.puf <- rowSums(PUF.1, na.rm = TRUE)
 # Normalize each row so sum = 1
-prof.puf.conc <- sweep(PUF.1, 1, row_totals, FUN = "/")
+prof.puf.conc <- sweep(PUF.1, 1, tmp.puf, FUN = "/")
 # Check
 rowSums(prof.puf.conc)  # should be all 1
-
 
 # (2) WBs
 tmp.wb <- rowSums(WB.1, na.rm = TRUE)
@@ -51,15 +52,14 @@ rowSums(prof.wb, na.rm = TRUE)
 df_long <- prof.puf.conc %>%
   mutate(obs = 1:n()) %>%
   pivot_longer(
-    cols = matches("^PCB"),   # matches any column starting with "PCB"
+    cols = matches("^PCB"),
     names_to = "PCB",
     values_to = "concentration"
   ) %>%
   filter(!is.na(concentration)) %>%
   mutate(
-    PCB = factor(PCB, levels = unique(PCB))  # keeps original order
+    PCB = factor(PCB, levels = unique(PCB))
   )
-
 
 df_summary <- df_long %>%
   group_by(PCB) %>%
@@ -71,19 +71,19 @@ df_summary <- df_long %>%
 
 p.puf <- ggplot(df_summary, aes(x = PCB, y = mean_conc)) +
   geom_col(
-    fill = "steelblue",
+    fill = "blue",
     color = "black",
     linewidth = 0.2
   ) +
-  geom_errorbar(aes(ymin = mean_conc,  # start from mean
-                    ymax = mean_conc + sd_conc),  # extend only upwards
+  geom_errorbar(aes(ymin = mean_conc,
+                    ymax = mean_conc + sd_conc),
                 width = 0.2,
                 color = "black") +
   theme_bw() +
   theme(
     aspect.ratio = 3/20,
-    axis.text.y = element_text(face = "bold", size = 12),
-    axis.title.y = element_text(face = "bold", size = 13),
+    axis.text.y = element_text(face = "bold", size = 10),
+    axis.title.y = element_text(face = "bold", size = 10),
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     panel.grid.major = element_blank(),
@@ -91,7 +91,16 @@ p.puf <- ggplot(df_summary, aes(x = PCB, y = mean_conc)) +
   ) +
   labs(
     x = NULL,
-    y = "Mean conc. fraction ",
+    y = expression(bold("Conc. Fraction " * Sigma * "PCB"))
+  ) +
+  annotate(
+    "text",
+    x = 145,           # choose x-position (PCB number or factor level)
+    y = 0.15,         # choose y-position
+    label = "Active low-volume sampler", 
+    color = "black", 
+    size =3,
+    fontface = "bold"
   )
 
 p.puf
@@ -113,7 +122,6 @@ df_long <- prof.wb %>%
     PCB = factor(PCB, levels = unique(PCB))  # keeps original order
   )
 
-
 df_summary <- df_long %>%
   group_by(PCB) %>%
   summarise(
@@ -124,7 +132,7 @@ df_summary <- df_long %>%
 
 p.wb <- ggplot(df_summary, aes(x = PCB, y = mean_conc)) +
   geom_col(
-    fill = "steelblue",
+    fill = "orange",
     color = "black",
     linewidth = 0.2
   ) +
@@ -135,8 +143,8 @@ p.wb <- ggplot(df_summary, aes(x = PCB, y = mean_conc)) +
   theme_bw() +
   theme(
     aspect.ratio = 3/20,
-    axis.text.y = element_text(face = "bold", size = 12),
-    axis.title.y = element_text(face = "bold", size = 13),
+    axis.text.y = element_text(face = "bold", size = 10),
+    axis.title.y = element_text(face = "bold", size = 10),
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     panel.grid.major = element_blank(),
@@ -144,11 +152,26 @@ p.wb <- ggplot(df_summary, aes(x = PCB, y = mean_conc)) +
   ) +
   labs(
     x = NULL,
-    y = "Mean conc. fraction ",
+    y = expression(bold("Conc. Fraction " * Sigma * "PCB"))
+  ) +
+  annotate(
+    "text",
+    x = 160,           # choose x-position (PCB number or factor level)
+    y = 0.15,         # choose y-position
+    label = "Static WB", 
+    color = "black", 
+    size =3,
+    fontface = "bold"
   )
 
 p.wb
 
 # Save plot in folder
-ggsave("Output/Plots/Profiles/Study1/PUFConcentration.png", plot = p.puf,
+ggsave("Output/Plots/Profiles/Study1/WB.png", plot = p.wb,
        width = 10, height = 3, dpi = 500)
+
+# Calculate cosine theta --------------------------------------------------
+# Add a column indicating sample type
+prof.puf.conc$type <- "PUF"
+prof.wb$type <- "WB"
+
